040fa4076eb2cb2c58e6abd282d3239862a5e631
David Henriquez <davidehenriquez@gmail.com>
2018-06-06 14:25:43 -0400

    add start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+mkdir ~/.copyVim
+cp -R ~/.vim ~/.copyVim
+cp ~/.vimrc ~/.copyVim
+rm -rf ~/.vim
+rm ~/.vimrc
+git clone https://github.com/mangoceylon/dotfiles ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+cd ~/.vim
+git submodule init
+git submodule update
+
